28edc6c957ec2684e1f56e1ed82a86bdae31b983
amit kumar gupta <amitgupta.gwl@gmail.com>
2024-10-19 06:34:24 +0530

    demo: handle keyboard events support tab and enter key

--- src/routes/dynamic/+page.svelte
+++ src/routes/dynamic/+page.svelte
@@ -2,6 +2,7 @@
   // import { onMount } from 'svelte';
   import SlimoChart from '$lib/SlimoChart.svelte';
   let initialAlgo = `
+
 FLOW: passed as parameter
 here you go
 IF go in loop
@@ -17,8 +18,40 @@ finsh here
   `
   let flowText = '';
   
+  function handleKeyDown(event) {
+    const textarea = event.target;
+    let text = textarea.value;  // Bound to the textarea
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const beforeCursor = text.substring(0, start);
+      const afterCursor = text.substring(end);
+      // console.log(beforeCursor.length)
+      const previousLine = beforeCursor.split("\n").pop() || "";
+      const leadingSpaces = previousLine.match(/^\s*/)[0] || "";
+
+      textarea.value = `${beforeCursor}\n${leadingSpaces}${afterCursor}`;
+      const newCursorPosition = beforeCursor.length + 1 + leadingSpaces.length;
+      textarea.setSelectionRange(newCursorPosition, newCursorPosition);
+    }
+    else if (event.key === "Tab") {
+      event.preventDefault();
+      // const TAB_SIZE = 2;
+      const spaces = "  ";
+
+      const beforeCursor = text.substring(0, start);
+      const afterCursor = text.substring(end);
+
+      textarea.value = `${beforeCursor}${spaces}${afterCursor}`;
+      const newCursorPosition = beforeCursor.length + spaces.length;
+      textarea.setSelectionRange(newCursorPosition, newCursorPosition);
+    }
+  }
   function handleKeyUp(event) {
     flowText = event.target.value;
+    
   }
 $: flowText = initialAlgo;
 </script>
@@ -37,9 +70,11 @@ $: flowText = initialAlgo;
   <textarea 
     class="text-area" 
     on:keyup={handleKeyUp} 
+    on:keydown={handleKeyDown} 
+    
     >{initialAlgo}
 
   </textarea>
   
-  <SlimoChart style="padding-left:10px; width:65vw; height:100vh" text={flowText} />
+  <SlimoChart style="padding-left:10px; width:65vw; height:100vh" bind:text={flowText} />
 </div>
