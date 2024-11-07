1df87b42c35bd0a5f95e106ab9d9b040f75dc8e7
amit kumar gupta <amitgupta.gwl@gmail.com>
2024-11-07 08:24:16 +0530

    static deploy site to launch on solothought.com

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapterStatic({
+			strict: false, //to ignore prerender static route error
+			// Default options to output static files in the "build" folder
+      pages: 'build',
+      assets: 'build',
+      fallback: null  // Use this if you want to configure a fallback page (e.g., for SPA behavior)
+		}),
+		paths:{
+			base:""
+		}
 	}
 };
 
