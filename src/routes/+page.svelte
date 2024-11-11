835405c41eb4bdeaa58bf9fb3ff3e037594ca60c
amit kumar gupta <amitgupta.gwl@gmail.com>
2024-11-11 11:44:08 +0530

    update page style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <main>
 <div class="container">
-  <h1>Text 2 Chart </h1>
+  <h1><img src="Text2Chart_logo.webp" alt="Text2Chart logo" style="width: 50px; padding: 5px;"/>Text 2 Chart </h1>
   <p class="">
     With respect to all existing text-to-diagram libraries, this library shares a similar purpose: building diagrams from text. By using text-based inputs, you gain the advantage of version control, enabling you to track changes, integrate with approval cycles, and share diagrams without concerns about file size.
   </p>
@@ -8,28 +8,26 @@
     Our library adds a unique focus on interactivity, creating diagrams that remain manageable and navigable as they expand. Currently, we’re launching FlowChart and have plan to add more type of diagrams on your feedback and requests. Follow and bookmark our GitHub repository to stay updated on new diagrams and enhanced features.
   </p>
   <br/>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h4 class="card-title">FlowChart</h4>
-            <p class="card-text">Draw a flow chart from an algorithm. Highlight nodes, edges, and path in different directions. Export as an image, Toggle the node detail and much more.</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item"><a href="/text2chart/flow">Online Application</a></li>
-            <li class="list-group-item"><a href="https://github.com/solothought/text2chart">Github Repository</a></li>
-            <li class="list-group-item"><a href="https://www.npmjs.com/package/@solothought/text2chart">NPM package</a></li>
-            <li class="list-group-item"><a href="https://solothought.com/tutorial/flowchart-from-text/">How to Guide</a></li>
-          </ul>
+  <div class="row">
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h4 class="card-title">FlowChart</h4>
+          <p class="card-text">Draw a flow chart from an algorithm. Highlight nodes, edges, and path in different directions. Export as an image, Toggle the node detail and much more.</p>
         </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><a href="/text2chart/flow">Online Application</a></li>
+          <li class="list-group-item"><a href="https://github.com/solothought/text2chart">Github Repository</a></li>
+          <li class="list-group-item"><a href="https://www.npmjs.com/package/@solothought/text2chart">NPM package</a></li>
+          <li class="list-group-item"><a href="https://solothought.com/tutorial/flowchart-from-text/">How to Guide</a></li>
+        </ul>
       </div>
-      <div class="col">
-        <!-- One of three columns -->
-      </div>
-      <div class="col">
-        <!-- One of three columns -->
-      </div>
+    </div>
+    <div class="col">
+      <!-- One of three columns -->
+    </div>
+    <div class="col">
+      <!-- One of three columns -->
     </div>
   </div>
 </div>
