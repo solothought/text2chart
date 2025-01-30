c453aabf7b97fff7b64006c38eef77719cd9adc7
amit kumar gupta <amitgupta.gwl@gmail.com>
2025-01-30 10:20:44 +0530

    UX improvements
    - icons are stored in separate files
    - icons position is changes out from step box
    - width of step text is increased
    - pointer cursor for FOLLOW step
    - replaced emoji with SVG
    -

--- src/lib/flow/Step.svelte
+++ src/lib/flow/Step.svelte
@@ -2,6 +2,10 @@
   import { Handle, Position } from '@xyflow/svelte';
   import { onMount, afterUpdate } from 'svelte';
   import { nodeSize } from './../config.js';
+  import BranchIcon from './../icons/branch.svelte';
+  import LoopIcon from './../icons/loop.svelte';
+  import ErrIcon from './../icons/error.svelte';
+  import LinkExternalIcon from './../icons/linkExternal.svelte';
 
   export const fontSize = {desire: 15, min: 10, max: 80}
 
@@ -46,24 +50,32 @@
   export let id;
 </script>
 
-<div class="{data.highlight ? 'hovered' : ''} {data.isEnd ? 'endStep':'step'}">
-  <div style="position: absolute;">
-    {#if data.type === "LOOP"}
-      <svg class="icon {data.isEnd ? 'invert':''}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="49px" height="42px" viewBox="-0.5 -0.5 49 42"><g><g data-cell-id="0"><g data-cell-id="1"><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-2"><g><path d="M 44 23.11 C 44 34.65 35.05 44 24 44 C 12.95 44 4 34.65 4 23.11 C 4 11.57 12.95 2.22 24 2.22" fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" transform="rotate(90,24,20.5)" pointer-events="all"/><path d="M 24 -3 L 31.5 2.22 L 24 7.44 Z" fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" transform="rotate(90,24,20.5)" pointer-events="all"/></g></g><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-5"/><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-6"/><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-3"/><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-4"/><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-8"/></g></g></g></svg>    
-    {/if}
-    {#if data.type === "IF" || data.type === "ELSE_IF"}
-      <svg class="icon {data.isEnd ? 'invert':''}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="78px" height="112px" viewBox="-0.5 -0.5 78 112"><g><g data-cell-id="0"><g data-cell-id="1"><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-2"/><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-5"><g><path d="M 11 21 Q 11 61 57 61" fill="none" stroke="rgb(0, 0, 0)" stroke-width="2" stroke-miterlimit="10" pointer-events="stroke"/></g></g><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-6"><g><path d="M 11 21 L 11 91" fill="none" stroke="rgb(0, 0, 0)" stroke-width="2" stroke-miterlimit="10" pointer-events="stroke"/></g></g><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-3"><g><ellipse cx="11" cy="11" rx="10" ry="10" fill="none" stroke="rgb(0, 0, 0)" stroke-width="2" pointer-events="all"/></g></g><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-4"><g><ellipse cx="67" cy="61" rx="10" ry="10" fill="none" stroke="rgb(0, 0, 0)" stroke-width="2" pointer-events="all"/></g></g><g data-cell-id="cL7QPJKOe-qMU4ihb_zf-8"><g><ellipse cx="11" cy="101" rx="10" ry="10" fill="none" stroke="rgb(0, 0, 0)" stroke-width="2" pointer-events="all"/></g></g></g></g></g></svg>
-    {/if}
-    {#if data.type === "ERR" }
-      <span style="font-size: 1.25em;">⛔</span>
-    {/if}
-    {#if data.type === "FOLLOW" }
-      <span style="font-size: 1.5em;">↖</span>
-    {/if}
-  </div>
+<div style="position: absolute;">
+  {#if data.type === "LOOP"}
+    <div class="step-icon">
+      <LoopIcon/>
+    </div>
+  {/if}
+  {#if data.type === "IF" || data.type === "ELSE_IF"}
+    <div class="step-icon">
+      <BranchIcon/>
+    </div>
+  {/if}
+  {#if data.type === "ERR" }
+    <div class="step-icon">
+      <ErrIcon/>
+    </div>
+  {/if}
+  {#if data.type === "FOLLOW" }
+    <div class="step-icon">
+      <LinkExternalIcon/>
+    </div>
+  {/if}
+</div>
+<div class="{data.highlight ? 'hovered' : ''} {data.isEnd ? 'endStep':'step'} {data.type === 'FOLLOW' ? 'clickable' : ''}">
   <!-- <Handle class="hndl" id="tt" type="target" position={Position.Top} /> -->
   <Handle class="hndl" id="t" type="target" position={Position.Top} />
-  <div class="container" style="width:{nodeSize.width}px; height:{nodeSize.height}px;" bind:this={containerElement}>
+  <div class="step-container" style="width:{nodeSize.width}px; height:{nodeSize.height}px;" bind:this={containerElement}>
     <div class="msgWrapper">
       <p bind:this={textElement}>{data.hideMsgDetail ? data.msg : data.rawMsg}</p>
     </div>
@@ -85,6 +97,9 @@
   :global(.hndl) {
     visibility: hidden;
   }
+  .clickable{
+    cursor: pointer;
+  }
   .endStep{
     padding: 5px;
     border-radius: 5px;
@@ -101,7 +116,7 @@
   }
 
 
-  .container {
+  .step-container {
     position: relative;
     /* width: 150px;
     height: 50px; */
@@ -109,17 +124,15 @@
   }
 
   .hovered {
-      border: 2px solid #FF4000;
-      background-color: lightgrey;
-      color: inherit;
-   }
+    border: 2px solid #FF4000;
+    background-color: lightgrey;
+    color: inherit;
+  }
 
-   .icon{
+  .step-icon{
     width: 25px;
     height: 25px;
-   }
-
-   svg.invert {
-    filter: invert(1); /* Invert the colors of the SVG */
+    top: -35px;
+    position: absolute;
   }
 </style>
---
+++ src/lib/icons/error.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let size = "25";
+  // export let height = "25px";
+</script>
+
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 50 50" xml:space="preserve">
+<circle style="fill:#D75A4A;" cx={size} cy={size} r={size}/>
+<polyline style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;" points="16,34 25,25 34,16 
+	"/>
+<polyline style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;" points="16,16 25,25 34,34 
+	"/>
+</svg>
